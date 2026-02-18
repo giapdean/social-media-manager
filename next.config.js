4ff465c8d494355a70243dded8f3facf49b1ab38
Giap <duc82122@gmail.com>
2026-02-18 19:30:53 +0700

    Simplify folder structure and add standalone output

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   env: {
     NEXT_PUBLIC_GAS_API_URL: process.env.NEXT_PUBLIC_GAS_API_URL,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
